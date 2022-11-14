f04e38c397aab116970622a0ef007ab945da9f62
Ardian Mulya <ardian.mulya.ft21@mail.umy.ac.id>
2022-11-14 16:51:52 +0700

    deklarasi variabel data no dan nama mahasiswa

--- double_linked_list/Program.cs
+++ double_linked_list/Program.cs
@@ -8,6 +8,12 @@ namespace double_linked_list
 {
     class Node
     {
+        /*Node class represents the node of doubly linked list.
+         it consist of the information part and links to
+         its succeefing and preceeding 
+         in terms of next and previous */
+        public int noMhs;
+        public string name;
 
     }
     class Program
